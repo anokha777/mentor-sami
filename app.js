444b753bb26bdecf56371da7b8337ef6afb6068d
Brajesh Anokha <anokha777@gmail.com>
2020-04-23 22:53:35 +0530

    testing heroku

---
+++ app.js
@@ -0,0 +1,44 @@
+import {} from "dotenv/config";
+import { createServer } from "http";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { createApolloServer } from "./apollo/index";
+import models from "./models";
+import types from "./types/index";
+import resolvers from "./resolvers";
+
+const { MONGO_URL, SERVER_PORT } = process.env;
+console.log("mongo url", typeof MONGO_URL);
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log(`Mongodb database connection successful`);
+  })
+  .catch((err) => {
+    console.error(`Error in databse connection ${err}`);
+  });
+
+const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.use('/test', (req, res) => {
+  res.send("Server is ok!!");
+})
+
+const server = createApolloServer(types, resolvers, models);
+server.applyMiddleware({ app, path: "/graphql" });
+
+// Create http server and add subscriptions to it
+const httpServer = createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+// Listen to HTTP and WebSocket server
+const port = SERVER_PORT;
+httpServer.listen({ port: port }, () => {
+  console.log(`server ready at port ${port}`);
+});
